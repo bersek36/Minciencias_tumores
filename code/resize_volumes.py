080eefb5131c2bc29200613afe39047399f84f66
Daniel Fernando Terraza Arciniegas <daniterfe@gmail.com>
2022-06-09 10:31:45 -0500

    update

--- code/resize_volumes.py
+++ code/resize_volumes.py
@@ -4,7 +4,10 @@ import itertools
 import os
 from multiprocessing import Pool
 from timeit import default_timer as timer
-def resize_data(data):
+
+
+from Clasification.test1 import *
+def resize_data(data, msk):
     initial_size_x = data.shape[0]
     initial_size_y = data.shape[1]
     initial_size_z = data.shape[2]
@@ -22,7 +25,12 @@ def resize_data(data):
     for x, y, z in itertools.product(range(new_size_x),
                                      range(new_size_y),
                                      range(new_size_z)):
-        new_data[x][y][z] = data[int(x * delta_x)][int(y * delta_y)][int(z * delta_z)]
+        if msk:
+            new_data[x][y][z] = round(data[int(x * delta_x)][int(y * delta_y)][int(z * delta_z)])
+            if new_data[x][y][z] not in [0,1,2,3,4]:
+                print(new_data[x][y][z])
+        else:
+            new_data[x][y][z] = data[int(x * delta_x)][int(y * delta_y)][int(z * delta_z)]
 
     return new_data.astype(np.int16)
 
@@ -43,16 +51,23 @@ def resize(i):
     msk = "E:\BRATS\BraTS2020_TrainingData\MICCAI_BraTS2020_TrainingData\BraTS20_Training_{}\BraTS20_Training_{}_seg.nii".format(num, num)
     out_file = "E:\BRATS\BraTS2020_TrainingData\\resized\BraTS20_Training_{}".format(num)
     os.makedirs(out_file)
+    
+    #"""
     initial_t1 = nib.load(t1).get_fdata()
     initial_msk = nib.load(msk).get_fdata()
     #if initial_data.shape != (256, 256, 192):
-    resized_t1 = resize_data(initial_t1)
-    resized_msk = resize_data(initial_msk)
+    resized_t1 = resize_data(initial_t1, False)
+    resized_msk = resize_data(initial_msk, True)
+    """
+    resized_t1 = process_scan(t1)
+    resized_msk = process_scan(msk)
+    """
     img_t1 = nib.Nifti1Image(resized_t1, np.eye(4))
     img_msk = nib.Nifti1Image(resized_msk, np.eye(4))
-    img_t1.to_filename(out_file + "\BraTS20_Training_{}_t1.nii".format(num))
-    img_msk.to_filename(out_file + "\BraTS20_Training_{}_seg.nii".format(num))
-
+    
+    img_t1.to_filename(out_file + "\BraTS20_Training_{}_t1.nii.gz".format(num))
+    img_msk.to_filename(out_file + "\BraTS20_Training_{}_seg.nii.gz".format(num))
+    
 
 if __name__ == '__main__':
     numeros =  list(range(0, 369))
